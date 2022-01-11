bc006b515ac748760e5f2e8e22a1286517a34556
shaggy611 <sevczik11@gmail.com>
2022-01-11 22:59:19 +0200

    lesson

--- app.js
+++ app.js
@@ -8,19 +8,31 @@ const App = {
         }
     },
     methods: {
-        inputCHangeHandler(event) {
-            this.inputValue = event.target.value
-        },
         addNewNote() {
             if(this.inputValue !== '') {
                 this.notes.push(this.inputValue)
                 this.inputValue = ''
             }
         },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
         deleteNoteItem(index) {
             this.notes.splice(index, 1)
         }
         
+    },
+    computed: {
+        doubleCount() {
+            return this.notes.length * 2
+        }
+    },
+    watch: { 
+        inputValue(value) {
+            if(value.length > 10) {
+                this.inputValue = ''
+            }
+        }
     }
 }
 
